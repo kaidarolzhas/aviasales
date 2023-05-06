6b18b013abb47ed8549f7c74146978b85b0719f5
Tan3103 <tannur.uataev@mail.ru>
2023-05-06 12:21:25 +0600

    Initial commit

---
+++ src/test/java/com/ticket/aviasales/AviasalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.aviasales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviasalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
